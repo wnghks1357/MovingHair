cba643b4ed89fbc54addfed3025b5c17823e5b9e
JOOHWAN <wnghks1357@gmail.com>
2019-04-09 23:18:15 +0900

    add uservo, and java version modify from 1.6 to 1.8

--- src/main/java/com/moving/vo/UserVo.java
+++ src/main/java/com/moving/vo/UserVo.java
@@ -1,9 +1,32 @@
 package com.moving.vo;
 
+import java.sql.Timestamp;
+
 import lombok.Data;
 
 @Data
 public class UserVo {
-
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private char userType;
+	private String userName;
+	private String userPhone;
+	private String userEmail;
+	private char userStatus;
+	private String userZip;
+	private String userAddr1;
+	private String userAddr2;
+	private int userBirthday;
+	private char userSex;
+	private char receiveEmail;
+	private char receiveSms;
+	private char createdType;
+	private char requiredAgree;
+	private char secretAgree;
+	private Timestamp createdDate;
+	private Timestamp updateDate;
+	private Timestamp outDate;
+	private Timestamp loginDate;
 	
 }
